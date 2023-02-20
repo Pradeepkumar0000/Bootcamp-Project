05277d4fa1adf1c58477bf174bc16cb2545f5935
Pradeep Kumar <pardeep9536757207@gmail.com>
2023-02-20 07:52:24 +0530

    basic setup and colors

--- middlewears/logger.js
+++ middlewears/logger.js
@@ -3,10 +3,10 @@ const express = require('express');
 
 // Logger middleware
 const logger = (req, res, next) =>{
-    console.log(req.method) // get method, put method, post method, delete method
-    console.log(req.protocol) // http or https
-    console.log(req.get('host')) // localhost:5000
-    console.log(req.originalUrl) // /api/v1/bootcamp
+    // console.log(req.method) // get method, put method, post method, delete method
+    // console.log(req.protocol) // http or https
+    // console.log(req.get('host')) // localhost:5000
+    // console.log(req.originalUrl) // /api/v1/bootcamp
     console.log(`Url: ${req.protocol}://${req.get('host')}${req.originalUrl}`)
     next()
 }
